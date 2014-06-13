f8994d211761191e8ffd8693807db4c20931372c
mjbondra <mjbondra@gmail.com>
2014-06-12 23:24:34 -0400

    update client-side views and routes

--- client/config/routes.js
+++ client/config/routes.js
@@ -9,24 +9,50 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
   $locationProvider.html5Mode(true);
   $locationProvider.hashPrefix('!');
 
+  // static page routes
   $routeProvider.when('/about', {
     templateUrl: '/app/views/about/index.html',
     controller: 'about'
   });
-
   $routeProvider.when('/contact', {
     templateUrl: '/app/views/contact/index.html',
     controller: 'contact'
   });
 
+  // project routes
   $routeProvider.when('/projects', {
     templateUrl: '/app/views/projects/index.html',
     controller: 'projects.index'
   });
+  $routeProvider.when('/projects/new', {
+    templateUrl: '/app/views/projects/new.html',
+    controller: 'projects.new'
+  });
   $routeProvider.when('/projects/:project', {
     templateUrl: '/app/views/projects/show.html',
-    controller: 'project.show'
+    controller: 'projects.show'
   });
+  $routeProvider.when('/projects/:project/edit', {
+    templateUrl: '/app/views/projects/edit.html',
+    controller: 'projects.edit'
+  });
+
+  // user/session routes
+  $routeProvider.when('/users/:username', {
+    templateUrl: '/app/views/users/edit.html',
+    controller: 'users.edit'
+  });
+  $routeProvider.when('/users/sessions/new', {
+    templateUrl: '/app/views/users/sessions/new.html',
+    controller: 'users.sessions.new'
+  });
+  // $routeProvider.when('/users/sessions/destroy', {
+  //   resolve: {
+  //     response: ['User', function (User) {
+  //       User.signOut();
+  //     }]
+  //   }
+  // });
 
   // default route
   $routeProvider.otherwise({
