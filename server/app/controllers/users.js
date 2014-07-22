2039a67cfc0d898fba35b1dff2675c57f5189601
mjbondra <mjbondra@gmail.com>
2014-07-21 20:58:32 -0400

    add additional functionality for users

--- server/app/controllers/users.js
+++ server/app/controllers/users.js
@@ -9,9 +9,83 @@ var coBody = require('co-body')
   , Promise = require('bluebird')
   , _ = require('underscore');
 
+/**
+ * Models
+ */
+var User = mongoose.model('User');
+
+/**
+ * Vars
+ */
+var blacklist = [ '__v' ]
+  , projection = { __v: 0 };
+
 module.exports = {
-  update: function *(next) {
+  findOne: function *(next) {
+    this.user = yield Promise.promisify(User.findOne, User)({ slug: this.params.user });
     yield next;
+    delete this.user;
+  },
+
+  /**
+   * Index
+   * GET /api/users
+   */
+  index: function *(next) {
+    this.body = yield Promise.promisify(User.find, User)({}, projection);
+  },
+
+  /**
+   * Show
+   * GET /api/users/:user
+   */
+  show: function *(next) {
+    if (!this.user) return yield next; // 404 Not Found
+    this.body = yield cU.censor(this.user, blacklist);
+  },
+
+  /**
+   * Create
+   * POST /api/users
+   */
+  create: function *(next) {
+    this.user = new User(_.omit(yield coBody(this), 'images'));
+    yield Promise.promisify(this.user.save, this.user)();
+    this.status = 201; // 201 Created
+    this.body = yield cU.created('user', this.user);
+  },
+
+  /**
+   * Update
+   * POST /api/users/:user
+   */
+  update: function *(next) {
+    if (!this.user) return yield next; // 404 Not Found
+    this.user = _.extend(this.user, yield cU.censor(yield coBody(this), ['encoding', 'files', 'mimetype', 'type']));
+    yield Promise.promisify(this.user.save, this.user)();
+    this.body = yield cU.updated('user', this.user);
+  },
+
+  /**
+   * Destroy
+   * DELETE /api/users/:user
+   */
+  destroy: function *(next) {
+    if (!this.user) return yield next; // 404 Not Found
+    yield Promise.promisify(this.user.remove, this.user)();
+    this.body = yield cU.deleted('user', this.user);
+  },
+
+  images: {
+    create: function *(next) {
+      yield next;
+    },
+    update: function *(next) {
+      yield next;
+    },
+    destroy: function *(next) {
+      yield next;
+    }
   },
   sessions: {
     show: function *(next) {
--- server/config/routes.js
+++ server/config/routes.js
@@ -8,7 +8,8 @@ var send = require('koa-send');
  * Controllers
  */
 var projects = require('../app/controllers/projects')
-  , snippets = require('../app/controllers/snippets');
+  , snippets = require('../app/controllers/snippets')
+  , users = require('../app/controllers/users');
 
 module.exports = function (app, config) {
 
@@ -28,6 +29,13 @@ module.exports = function (app, config) {
   app.get('/api/snippets/:snippet', snippets.findOne, snippets.show);
   app.put('/api/snippets/:snippet', snippets.findOne, snippets.update);
   app.delete('/api/snippets/:snippet', snippets.findOne, snippets.destroy);
+
+  // users
+  app.get('/api/users', users.index);
+  app.post('/api/users', users.create);
+  app.get('/api/users/:user', users.findOne, users.show);
+  app.put('/api/users/:user', users.findOne, users.update);
+  app.delete('/api/users/:user', users.findOne, users.destroy);
 
   // redirect all remaining GET method routes to angular router
   app.get(/.*/, function *() {
