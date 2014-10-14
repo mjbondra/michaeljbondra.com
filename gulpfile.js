2669df366c8a8839e9c080da7c9840bac4087d28
mjbondra <mjbondra@gmail.com>
2014-10-14 16:33:48 -0400

    add bower to watch task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ require('./.gulp/css');   //  task(s):  css, css-install
 require('./.gulp/js');    //  task(s):  js, js-app, js-libraries, js-install
 
 gulp.task('watch', function () {
+  gulp.watch(['./bower.json'], ['bower']);
   gulp.watch(['./client/ng/*.js', './client/ng/**/*.js'], ['js-app']);
   gulp.watch(['./client/scss/*.scss', './client/scss/**/*.scss'], ['css']);
 });
