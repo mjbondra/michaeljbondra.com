4d2578019e08ebeaa6d304a3023288afad700456
mjbondra <mjbondra@gmail.com>
2014-11-18 22:51:53 -0500

    add new projects resources

---
+++ client/ng/components/projects/controllers.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var angular = require('angular')
+, app = angular.module('mjbondra.components.projects.controllers', []);
+
+app.controller('projects.index', [function () {}]);
+app.controller('projects.show', [function () {}]);
