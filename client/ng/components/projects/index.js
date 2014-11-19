74e338b16f83693e698b6eef65235f68a1b34105
mjbondra <mjbondra@gmail.com>
2014-11-18 22:55:02 -0500

    connect new projects controller

--- client/ng/components/projects/index.js
+++ client/ng/components/projects/index.js
@@ -2,10 +2,12 @@
 
 var angular = require('angular');
 
+require('./controllers');
 require('./directives');
 require('./services');
 
 angular.module('mjbondra.components.projects', [
+  'mjbondra.components.projects.controllers',
   'mjbondra.components.projects.directives',
   'mjbondra.components.projects.services'
 ]);
