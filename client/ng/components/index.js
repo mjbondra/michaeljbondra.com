677b802f2bcf9cd9038b37d037235401685b97bf
mjbondra <mjbondra@gmail.com>
2014-11-18 22:27:39 -0500

    connect details component

--- client/ng/components/index.js
+++ client/ng/components/index.js
@@ -3,11 +3,13 @@
 var angular = require('angular');
 
 require('./console');
+require('./details');
 require('./google-analytics');
 require('./projects');
 
 angular.module('mjbondra.components', [
   'mjbondra.components.console',
+  'mjbondra.components.details',
   'mjbondra.components.google-analytics',
   'mjbondra.components.projects'
 ]);
