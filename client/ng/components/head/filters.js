eacdaadabfe0919b5b78e251ef86e0ae9b1fd3cd
mjbondra <mjbondra@gmail.com>
2014-11-22 13:20:24 -0500

    update title filter

--- client/ng/components/head/filters.js
+++ client/ng/components/head/filters.js
@@ -4,7 +4,7 @@ var angular = require('angular')
 , app = angular.module('mjbondra.components.head.filters', []);
 
 app.filter('title', function () {
-  return function (input) {
-    return input ? input + ' | Michael J. Bondra' : 'Michael J. Bondra';
+  return function (title) {
+    return title ? title + ' | Michael J. Bondra' : 'Michael J. Bondra';
   };
 });
