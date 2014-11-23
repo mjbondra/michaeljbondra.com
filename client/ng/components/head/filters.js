e381822a9d7e307282356a3f0be4084326fcdb90
mjbondra <mjbondra@gmail.com>
2014-11-22 21:34:33 -0500

    update head filters; update primary template

--- client/ng/components/head/filters.js
+++ client/ng/components/head/filters.js
@@ -3,8 +3,18 @@
 var angular = require('angular')
 , app = angular.module('mjbondra.components.head.filters', []);
 
-app.filter('title', function () {
-  return function (title) {
-    return title ? title + ' | Michael J. Bondra' : 'Michael J. Bondra';
+app.filter('formatDescription', [function () {
+  return function (description) {
+    return description ?
+      description.trim().replace(/\r?\n|\r/g, '') :
+      '';
   };
-});
+}]);
+
+app.filter('formatTitle', [function () {
+  return function (title) {
+    return title ?
+      title + ' | Michael J. Bondra' :
+      'Michael J. Bondra';
+  };
+}]);
