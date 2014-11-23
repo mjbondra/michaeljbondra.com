2254309fdc6ec87f073d7a670d6ca29814ef9058
mjbondra <mjbondra@gmail.com>
2014-11-23 14:56:50 -0500

    support css minification with sourcemaps

--- .gulp/css.js
+++ .gulp/css.js
@@ -1,8 +1,10 @@
 var autoprefixer = require('autoprefixer-core')
+  , csswring = require('csswring')
   , gulp = require('gulp')
   , notify = require('gulp-notify')
   , plumber = require('gulp-plumber')
   , postcss = require('gulp-postcss')
+  , rename = require('gulp-rename')
   , sass = require('gulp-ruby-sass')
   , sourcemaps = require('gulp-sourcemaps');
 
@@ -17,15 +19,30 @@ function css () {
       require: 'sass-globbing',
       sourcemap: true
     })
-    .pipe(plumber())
-    .pipe(postcss(processors))
-    .pipe(sourcemaps.write('./', {
-      includeContent: false,
-      sourceRoot: '/scss'
-    }))
+      .pipe(plumber())
+      .pipe(postcss(processors))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./client/css/'))
     .pipe(notify('CSS was successfully compiled.'));
 }
 
+function cssMinify () {
+  var processors = [
+    csswring({
+      removeAllComments: true
+    })
+  ];
+  return gulp.src('./client/css/styles.css', { base: './client' })
+    .pipe(plumber())
+    .pipe(rename(function (path) { path.basename += '.min'; }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(postcss(processors))
+    .pipe(sourcemaps.write('./', {
+      sourceRoot: '../'
+    }))
+    .pipe(gulp.dest('./client'));
+}
+
 gulp.task('css', css);
+gulp.task('css-minify', ['css'], cssMinify);
 gulp.task('css-install', ['bower'], css);
