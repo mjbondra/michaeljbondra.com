e7b7deb5ac619eca9bb6acc79e5b89e77ae475df
mjbondra <mjbondra@gmail.com>
2014-11-25 22:31:42 -0500

    update project model

--- server/components/projects/model.js
+++ server/components/projects/model.js
@@ -4,14 +4,19 @@ var mongoose = require('mongoose')
 
 var ProjectSchema = new Schema({
   body: String,
+  color: String,
   github: String,
   image: {
-    thumbnail: String, // 150 x 150
     background: {
+      repeat: Boolean,
+      default: String, // 480 x 340
       desktop: String, // 1340 x 700
-      mobile: String, // 480 x 340
       mobileWide: String, // 767 x 340
       tablet: String // 1024 x 500
+    },
+    thumbnail: {
+      default: String, // 150 x 150
+      hiRes: String // 300 x 300
     }
   },
   slug: {
