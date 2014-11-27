689e5b0ec446b2d610020b1f66157eb9ec627076
mjbondra <mjbondra@gmail.com>
2014-11-26 23:08:00 -0500

    update projects controller and routes

--- server/components/projects/controllers.js
+++ server/components/projects/controllers.js
@@ -3,6 +3,14 @@ var mongoose = require('mongoose')
 
 var Project = mongoose.model('Project');
 
-exports.index = function *(next) {
+exports.index = function *() {
   this.body = yield Bluebird.promisify(Project.find, Project)();
+};
+
+exports.show = function *(next) {
+  var project = yield Bluebird.promisify(Project.findOne, Project)({
+    slug: this.params.project
+  });
+  if (!project) return yield next;
+  this.body = project;
 };
--- server/config/routes.js
+++ server/config/routes.js
@@ -4,5 +4,6 @@ var email = require('../components/messages/middleware').email
 
 module.exports = function (app) {
   app.get('/api/projects', projects.index);
+  app.get('/api/projects/:project', projects.show);
   app.post('/api/messages', email, messages.create);
 };
