067255525c11dff6ed6b7bcd14006288060b6ae9
mjbondra <mjbondra@gmail.com>
2014-11-28 22:41:00 -0500

    update project controller

--- client/ng/components/projects/controllers.js
+++ client/ng/components/projects/controllers.js
@@ -3,10 +3,19 @@
 var angular = require('angular')
   , app = angular.module('mjbondra.components.projects.controllers', []);
 
-app.controller('projects.index', ['$scope', 'Project', function ($scope, Project) {
-  $scope.projects = Project.query();
-}]);
+app.controller('projects.index', [
+  '$scope',
+  'Project',
+  function ($scope, Project) {
+    $scope.projects = Project.query();
+  }
+]);
 
-app.controller('projects.show', ['$routeParams', '$scope', 'Project', function ($routeParams, $scope, Project) {
-  $scope.project = Project.get({ project: $routeParams.project });
-}]);
+app.controller('projects.show', [
+  '$routeParams',
+  '$scope',
+  'Project',
+  function ($routeParams, $scope, Project) {
+    $scope.project = Project.get({ project: $routeParams.project });
+  }
+]);
