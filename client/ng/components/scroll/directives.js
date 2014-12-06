92625dbfb377c95139c5cb8ce0243d6f872de790
mjbondra <mjbondra@gmail.com>
2014-12-06 10:44:34 -0500

    update navigation scroll animations

--- client/ng/components/scroll/directives.js
+++ client/ng/components/scroll/directives.js
@@ -3,11 +3,57 @@
 var angular = require('angular')
   , app = angular.module('mjbondra.components.scroll.directives', ['duScroll']);
 
-app.directive('scrollTo', ['$document', function($document) {
+app.directive('scrollHome', ['$document', function ($document) {
   return {
     link: function (scope, element) {
+      var duration = 250; // ms
+
+      element.on('click', function () {
+        $document.scrollTop(0, duration);
+      });
+
+      scope.$on('$destroy', function () {
+        element.off('click');
+      });
+    }
+  };
+}]);
+
+app.directive('scrollOnClick', ['$document', function ($document) {
+  return {
+    link: function (scope, element, attributes) {
       var duration = 250 // ms
-        , offset = 30; // px
+        , offset = attributes.scrollOffset ?
+            parseInt(attributes.scrollOffset, 10) :
+            0;
+
+      element.on('click', function (e) {
+        e.preventDefault();
+
+        var targetId = attributes.href.replace(/.*(?=#[^\s]+$)/, '').slice(1)
+          , targetElement = document.getElementById(targetId);
+
+        $document.scrollToElement(
+          angular.element(targetElement),
+          offset,
+          duration
+        );
+      });
+
+      scope.$on('$destroy', function () {
+        element.off('click');
+      });
+    }
+  };
+}]);
+
+app.directive('scrollTo', ['$document', function ($document) {
+  return {
+    link: function (scope, element, attributes) {
+      var duration = 250 // ms
+        , offset = attributes.scrollOffset ?
+            parseInt(attributes.scrollOffset, 10) :
+            0; // px
 
       $document.scrollToElement(element, offset, duration);
     }
