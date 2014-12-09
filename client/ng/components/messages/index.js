d155af2e1b8b3f65b78238207641710d1744970f
mjbondra <mjbondra@gmail.com>
2014-12-08 23:08:34 -0500

    add message model to client app

--- client/ng/components/messages/index.js
+++ client/ng/components/messages/index.js
@@ -3,7 +3,9 @@
 var angular = require('angular');
 
 require('./directives');
+require('./model');
 
 angular.module('mjbondra.components.messages', [
-  'mjbondra.components.messages.directives'
+  'mjbondra.components.messages.directives',
+  'mjbondra.components.messages.model'
 ]);
---
+++ client/ng/components/messages/model.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var angular = require('angular')
+  , app = angular.module('mjbondra.components.messages.model', ['ngResource']);
+
+app.factory('Message', ['$resource', function ($resource) {
+  return $resource('api/messages/:message');
+}]);
