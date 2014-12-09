5907b2322f94a060583403ea260a01186d464c08
mjbondra <mjbondra@gmail.com>
2014-12-08 22:52:58 -0500

    update error middleware; add new tests for messages

--- server/components/errors/middleware.js
+++ server/components/errors/middleware.js
@@ -1,7 +1,42 @@
 var mongoose = require('mongoose')
-, Bluebird = require('bluebird');
+  , Bluebird = require('bluebird');
 
 var _Error = mongoose.model('Error');
+
+var errorActions = {
+  defaultError: function (err, ctx) {
+    ctx.status = err.status || 500;
+    ctx.body = {
+      msg: 'Internal server error',
+      status: ctx.status
+    };
+  },
+
+  RejectionError: function (err, ctx) { // shift rejection errors to their cause
+    var _err = err.cause ? err.cause : err;
+    this[this[_err.name] ? _err.name : 'defaultError'](_err, ctx);
+  },
+
+  ValidationError: function (err, ctx) {
+    var errorArray = []
+      , objKeys = Object.keys(err.errors);
+
+    for (var i = 0; i < objKeys.length; i++) {
+      errorArray.push({
+        msg: err.errors[objKeys[i]].message,
+        field: err.errors[objKeys[i]].path,
+        value: err.errors[objKeys[i]].value
+      });
+    }
+
+    ctx.status = 422; // 422 Unprocessable Entity
+    ctx.body = {
+      errors: errorArray,
+      msg: 'Validation error',
+      status: ctx.status
+    };
+  }
+};
 
 exports.error = function () {
   return function *(next) {
@@ -9,6 +44,11 @@ exports.error = function () {
       yield next;
     } catch (err) {
       try {
+        errorActions[errorActions[err.name] ?
+          err.name :
+          'defaultError'
+        ](err, this);
+
         var _error = new _Error({ // record errors as Mongoose-modeled documents
           ip: this.ip,
           method: this.method,
@@ -22,11 +62,6 @@ exports.error = function () {
         console.error(err.stack || err); // original error
         console.error(_err.stack || _err); // error-logging error
       }
-      this.status = err.status || 500;
-      this.body = {
-        msg: 'Internal server error',
-        status: this.status
-      };
     }
   };
 };
--- server/test/messages.js
+++ server/test/messages.js
@@ -6,6 +6,35 @@ var agent = supertest.agent(app.listen());
 
 describe('Messages', function () {
   describe('POST /api/messages', function () {
+    describe('Invalid parameters', function () {
+      describe('Missing body field', function () {
+        it('should return JSON and a 422', function (done) {
+          agent
+          .post('/api/messages')
+          .send({
+            email: 'foobar@example.com',
+            name: 'Foo Baz'
+          })
+          .expect('Content-Type', /json/)
+          .expect(422, done);
+        });
+      });
+
+      describe('Invalid email address', function () {
+        it('should return JSON and a 422', function (done) {
+          agent
+          .post('/api/messages')
+          .send({
+            body: 'Foo bar, foo baz.',
+            email: 'foobar',
+            name: 'Foo Baz'
+          })
+          .expect('Content-Type', /json/)
+          .expect(422, done);
+        });
+      });
+    });
+
     describe('Valid parameters', function () {
       it('should return JSON and a 201', function (done) {
         agent
