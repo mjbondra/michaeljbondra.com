4213fefb200a05f8873158d9208986556953ee15
mjbondra <mjbondra@gmail.com>
2014-12-10 00:27:22 -0500

    add messages form directive

--- client/ng/components/messages/directives.js
+++ client/ng/components/messages/directives.js
@@ -3,7 +3,7 @@
 var angular = require('angular')
   , app = angular.module('mjbondra.components.messages.directives', []);
 
-app.directive('messages', [function () {
+app.directive('messageForm', [function () {
   return {
     controller: 'messages.new',
     templateUrl: '/ng/components/messages/new.html'
